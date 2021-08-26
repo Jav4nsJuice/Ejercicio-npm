34836c8beb8ce10d01e0f3648a9a808bb89b615e
Jav4nsJuice <javans.ferrel@gmail.com>
2021-08-26 11:11:22 -0400

    Commit Rebanadas 3 Saludador con Genero

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 const nombre = document.querySelector('#nombre-input');
 const form = document.querySelector('#saludador-form');
 const form_edad = document.querySelector('#saludador-con-edad-form');
+const form_genero = document.querySelector('#saludador-con-genero-form');
 const edad = document.querySelector('#edad-input');
+const genero = document.querySelector('#genero-input');
 // alert(nombre.value);
 form.addEventListener("submit", (event) =>{
     alert("Hola: " + nombre.value + edad.value);
@@ -17,6 +19,43 @@ form_edad.addEventListener("submit", (event) =>{
         }
         else{
             alert("Hola Don/Doña " + nombre.value)
+        }
+    }    
+});
+
+form_genero.addEventListener("submit", (event) =>{
+    if(edad.value <= 18){
+        if(genero.value == "Masculino"){
+            alert("Hola Joven " + nombre.value);
+        }else{
+            if(genero.value == "Femenino"){
+                alert("Hola Señorita " + nombre.value);
+            }else{
+                alert("Genero invalido. Inserte Masculino o Femenino");
+            }
+        }
+    }else{
+        if(edad.value <=30){
+            if(genero.value == "Masculino"){
+                alert("Hola Señor " + nombre.value);
+            }else{
+                if(genero.value == "Femenino"){
+                    alert("Hola Señora " + nombre.value);
+                }else{
+                    alert("Genero invalido. Inserte Masculino o Femenino");
+                }
+            }
+        }
+        else{
+            if(genero.value == "Masculino"){
+                alert("Hola Don " + nombre.value);
+            }else{
+                if(genero.value == "Femenino"){
+                    alert("Hola Doña " + nombre.value);
+                }else{
+                    alert("Genero invalido. Inserte Masculino o Femenino");
+                }
+            }
         }
     }    
 });
