8351befa044c49be7eb936b4d95b2c726441fee7
Jav4nsJuice <javans.ferrel@gmail.com>
2021-08-26 11:00:09 -0400

    Commit Rebanadas 1 y 2

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
-alert("Presiona OK si estas list@ para empezar con javascript");
+// alert("Presiona OK si estas list@ para empezar con javascript");
+const nombre = document.querySelector('#nombre-input');
+const form = document.querySelector('#saludador-form');
+const form_edad = document.querySelector('#saludador-con-edad-form');
+const edad = document.querySelector('#edad-input');
+// alert(nombre.value);
+form.addEventListener("submit", (event) =>{
+    alert("Hola: " + nombre.value + edad.value);
+});
+
+form_edad.addEventListener("submit", (event) =>{
+    if(edad.value <= 18){
+        alert("Hola Joven/Señorita " + nombre.value);
+    }else{
+        if(edad.value <=30){
+            alert("Hola Señor/Señora " + nombre.value);
+        }
+        else{
+            alert("Hola Don/Doña " + nombre.value)
+        }
+    }    
+});
+
+
